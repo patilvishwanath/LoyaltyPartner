5d037b7cc20e157b544146d4101000f6ed70744c
Vishwanath Patil <vishwanath26.patil@gmail.com>
2023-01-26 19:22:10 +0530

    1. Added networking,coroutines,dagger hilt dependency 2. Added application class

---
+++ app/src/main/java/com/vp/loyaltypartner/LoyaltyPartnerApplication.java
@@ -0,0 +1,13 @@
+package com.vp.loyaltypartner;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+class LoyaltyPartnerApplication extends Application {
+  @Override
+  public void onCreate() {
+    super.onCreate();
+  }
+}
